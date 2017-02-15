6d08f7e219016248d6d7ba40cdc1dc7b57caa644
Mateusz Janda <mateusz.janda@gmail.com>
2017-02-15 16:38:53 +0100

    Refactor + lightning branches

--- lightning.py
+++ lightning.py
@@ -5,92 +5,130 @@ import curses
 from curses.textpad import Textbox, rectangle
 import random
 from time import sleep
+import collections
+
+Light = collections.namedtuple('Light', ['y', 'x', 'symbol'])
+
+
+def createLightning():
+    x = curses.COLS / 2 + random.randint(-10, 10)
+    y = 0
+    lightning = [Light(y, x, random.choice('/|\\'))]
+    branches = []
+    while y < curses.LINES - 1:
+        _, _, prev_symbol = lightning[-1]
+        if prev_symbol == '|':
+            y += 1
+            symbol = random.choice('/|\\')
+        elif prev_symbol == '/':
+            symbol = random.choice('/|\\_')
+            if symbol == '/' or symbol == '_':
+                x -= 1
+            if symbol != '_':
+                y += 1
+        elif prev_symbol == '\\':
+            symbol = random.choice('/|\\_')
+            if symbol == '\\' or symbol == '_':
+                x += 1
+            if symbol != '_':
+                y += 1
+        elif prev_symbol == '_':
+            if lightning[-1].x < lightning[-2].x:
+                symbol = random.choice('/_')
+                x -= 1
+            else:
+                symbol = random.choice('\\_')
+                x += 1
+            if symbol != '_':
+                y += 1
+
+        if random.randint(0, 10) == 1:
+            branches.append(createBranch(lightning[-1], Light(y, x, symbol)))
+
+        lightning.append(Light(y, x, symbol))
+
+    return lightning, branches
+
+
+def createBranch(prev, root):
+    branch = [prev, root]
+    y = root.y
+    x = root.x
+    for i in range(random.randint(15, 30)):
+        _, _, prev_symbol = branch[-1]
+        if prev_symbol == '|':
+            y += 1
+            symbol = random.choice('/\\')
+        elif prev_symbol == '/':
+            symbol = random.choice('/\\_')
+            if symbol == '/' or symbol == '_':
+                x -= 1
+            if symbol != '_':
+                y += 1
+        elif prev_symbol == '\\':
+            symbol = random.choice('/\\_')
+            if symbol == '\\' or symbol == '_':
+                x += 1
+            if symbol != '_':
+                y += 1
+        elif prev_symbol == '_':
+            if branch[-1].x < branch[-2].x:
+                symbol = random.choice('/_')
+                x -= 1
+            else:
+                symbol = random.choice('\\_')
+                x += 1
+            if symbol != '_':
+                y += 1
+
+        if x < 0 or x >= curses.COLS or y < 0 or y >= curses.LINES:
+            break
+        branch.append(Light(y, x, symbol))
+
+    del branch[0]
+    return branch
 
 
 def blink(lightning, attr1, attr2):
     for l in lightning:
-        y, x, symbol = l
-        stdscr.addstr(y, x, symbol, attr1)
+        scr.addstr(l.y, l.x, l.symbol, attr1)
 
     sleep(0.1)
-    stdscr.refresh()
+    scr.refresh()
 
     for l in lightning:
-        y, x, symbol = l
-        stdscr.addstr(y, x, symbol, attr2)
+        scr.addstr(l.y, l.x, l.symbol, attr2)
 
     sleep(0.1)
-    stdscr.refresh()
+    scr.refresh()
 
 
-def createNewSymbol(prev):
-    if prev == '|':
-        return random.choice('/|\\')
-    if prev == '_':
-        return random.choice('/\\_')
-    return random.choice('/|\\_')
-
-
-stdscr = curses.initscr()
-curses.halfdelay(5)           # How many tenths of a second are waited, from 1 to 255
-curses.noecho()               # Wont print the input
-curses.curs_set(0)
-
-stdscr.refresh()
+scr = curses.initscr()
+curses.halfdelay(5)       # How many tenths of a second are waited, from 1 to 255
+curses.noecho()           # Wont print the input
+curses.curs_set(False)
 
 
 while True:
     # This blocks (waits) until the time has elapsed, or there is input to be handled
-    char = stdscr.getch()
-    stdscr.clear()
+    char = scr.getch()
+    scr.clear()
 
     if char == ord('q'):
         break
 
-    lightning = []
-    x = curses.COLS / 2 + random.randint(-10, 10)
-    y = 0
-    while y < curses.LINES - 1:
-        if not lightning:
-            lightning.append((y, x, random.choice('/|\\')))
-        else:
-            _, _, prev_symbol = lightning[-1]
-            if prev_symbol == '|':
-                y += 1
-                lightning.append((y, x, createNewSymbol(prev_symbol)))
-            elif prev_symbol == '/':
-                symbol = createNewSymbol(prev_symbol)
-                if symbol == '/' or symbol == '_':
-                    x -= 1
-                if symbol != '_':
-                    y += 1
-                lightning.append((y, x, symbol))
-            elif prev_symbol == '\\':
-                symbol = createNewSymbol(prev_symbol)
-                if symbol == '\\' or symbol == '_':
-                    x += 1
-                if symbol != '_':
-                    y += 1
-                lightning.append((y, x, symbol))
-            elif prev_symbol == '_':
-                if lightning[-1][1] < lightning[-2][1]:
-                    symbol = random.choice('/_')
-                    x -= 1
-                else:
-                    symbol = random.choice('\\_')
-                    x += 1
-                if symbol != '_':
-                    y += 1
-                lightning.append((y, x, symbol))
+    lightning, branches = createLightning()
 
-        ly, lx, lsymbol = lightning[-1]
-        try:
-            stdscr.addstr(ly, lx, lsymbol)
-        except:
-            print ly, lx, curses.LINES, lsymbol
+    for bs in branches:
+        for b in bs:
+            scr.addstr(b.y, b.x, b.symbol)
 
-        # sleep(0.01)
-        stdscr.refresh()
+    for l in lightning:
+        scr.addstr(l.y, l.x, l.symbol)
+        sleep(0.01)
+        scr.refresh()
+
+    scr.refresh()
 
     blink(lightning, curses.A_BOLD, curses.A_NORMAL)
     blink(lightning, curses.A_BOLD, curses.A_NORMAL)
