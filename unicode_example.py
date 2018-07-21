f3a7acc6901b4160600b9b8e6c41bc230d3c4220
Mateusz Janda <mateusz.janda@gmail.com>
2018-07-21 17:36:26 +0200

    Refactor unicode_example.py

--- unicode_example.py
+++ unicode_example.py
@@ -1,38 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# import urwid
-
-# txt = urwid.Text(u"Hello World")
-# fill = urwid.Filler(txt, 'top')
-# loop = urwid.MainLoop(fill)
-# loop.run()
+import curses
+import locale
+import time
 
 """
 http://stackoverflow.com/questions/1279341/how-do-i-use-extended-characters-in-pythons-curses-library
 https://en.wikipedia.org/wiki/Braille_Patterns
 """
 
-import curses
-import locale
-
-locale.setlocale(locale.LC_ALL, '')    # set your locale
-
-scr = curses.initscr()
-scr.clear()
-scr.addstr(0, 0, u'\u3042'.encode('utf-8'))
-scr.addstr(1, 0, u'\u2875'.encode('utf-8'))
-# scr.addstr('asdf')
-scr.refresh()
-
-
-while True:
-    # This blocks (waits) until the time has elapsed, or there is input to be handled
-    char = scr.getch()
+def main(scr):
     scr.clear()
+    scr.addstr(0, 0, u'\u3042'.encode('utf-8'))
+    scr.addstr(1, 0, u'\u2875'.encode('utf-8'))
+    scr.addstr(2, 0, 'asdf')
+    scr.refresh()
 
-    if char == ord('q'):
-        break
+    while not check_exit_key(scr):
+        time.sleep(0.1)
 
-# here implement simple code to wait for user input to quit
-curses.endwin()
+    curses.endwin()
+
+
+def check_exit_key(scr):
+    ch = scr.getch()
+    return ch == ord('q')
+
+
+if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, '')
+    curses.wrapper(main)
