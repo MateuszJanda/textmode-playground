fe6fa78497593240acb7dfb6f445113918fe681c
Mateusz Janda <mateusz.janda@gmail.com>
2018-07-21 17:27:49 +0200

    Refactor link.py

--- line.py
+++ line.py
@@ -6,67 +6,52 @@ import time
 import curses
 import locale
 
-locale.setlocale(locale.LC_ALL, '')    # set your locale
 
-def main():
-    scr = setup()
+BLANK_BRAILLE = u'\u2800'
 
-    # screen_buf = []
-    # for _ in range(curses.LINES):
-    #     screen_buf.append((list(u'\u2800' * (curses.COLS - 1))))
 
-    # draw_line(screen_buf)
+def main(scr):
+    setup()
+    scr.clear()
 
-    r = 0
+    num = 0
     while True:
-
         screen_buf = []
         for _ in range(curses.LINES):
-            screen_buf.append((list(u'\u2800' * (curses.COLS - 1))))
+            screen_buf.append((list(BLANK_BRAILLE * (curses.COLS - 1))))
 
-        draw_line(screen_buf, r)
-        r += 1
+        draw_line(screen_buf, num)
+        num += 1
 
-        time.sleep(0.001)
-
-        if r > 40:
+        time.sleep(0.1)
+        if num > 40:
             break
-
 
         display(scr, screen_buf)
 
     time.sleep(2)
-    curses.endwin()             # Przywraca terminal do oryginalnych ustawień
+    curses.endwin()
 
 
 def setup():
-    scr = curses.initscr()
-    curses.start_color()        # Potrzebne do definiowania kolorów
-    curses.use_default_colors() # Używaj kolorów terminala
-    curses.halfdelay(5)         # Ile częśći sekundy czekamy na klawisz, od 1 do 255
-    curses.noecho()             # Nie drukuje znaków na wejściu
-    curses.curs_set(False)      # Wyłącza pokazywanie kursora
-    scr.clear()
-
-    return scr
+    curses.start_color()
+    curses.use_default_colors()
+    curses.halfdelay(5)
+    curses.noecho()
+    curses.curs_set(False)
 
 
-def draw_line(screen_buf, rrr):
-    """ y = 6x + 3, x w [0, 40]"""
-
-    # x < curses.COLS * 2
-    for x in range(rrr):
+def draw_line(screen_buf, num):
+    for x in range(num):
         y = 1 * x + 3
 
         if curses.LINES - 1 - int(y / 4) < 0:
             continue
-        # screen_buf[int(y / 4)][int(x / 2)] = u'x'
         uchar = ord(screen_buf[curses.LINES - 1 - int(y / 4)][int(x / 2)])
-        screen_buf[curses.LINES - 1 - int(y / 4)][int(x / 2)] = unichr(uchar | fun(y, x))
+        screen_buf[curses.LINES - 1 - int(y / 4)][int(x / 2)] = unichr(uchar | relative_uchar(y, x))
 
 
-
-def fun(y, x):
+def relative_uchar(y, x):
     bx = x % 2
     by = y % 4
 
@@ -87,11 +72,10 @@ def display(scr, screen_buf):
 
     for num, line in enumerate(screen_buf):
         scr.addstr(num, 0, u''.join(line).encode('utf-8'))
-        # scr.addstr(num, 0, 'asdf')n)
 
     scr.refresh()
 
 
 if __name__ == '__main__':
-    main()
-
+    locale.setlocale(locale.LC_ALL, '')
+    curses.wrapper(main)
