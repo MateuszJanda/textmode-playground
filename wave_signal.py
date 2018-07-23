e7f424ed256d7ff6f799c2d7fad1c106e35c1547
Mateusz Janda <mateusz.janda@gmail.com>
2018-07-23 20:32:40 +0200

    Added drawing functions

--- wave_signal.py
+++ wave_signal.py
@@ -12,31 +12,35 @@
 unknown
 """
 
+from __future__ import print_function
+import sys
 import curses
 import math
 import random
 from time import sleep
+import collections
 import locale
 
 
 BLANK_BRAILLE = u'\u2800'
+
+Point = collections.namedtuple('Point', ['x', 'y'])
 
 
 def main(scr):
     setup_curses()
     scr.clear()
 
-    buf = clean_buf()
+    screen_buf = clean_buf()
     x = 0
 
     while not check_exit_key(scr):
-        shift_buf(buf)
-        draw_pt(buf, x)
-        show(buf)
+        shift_buf(screen_buf)
+        draw_amp(screen_buf, x)
+        show(scr, screen_buf)
 
         x += 1
         sleep(0.1)
-        scr.refresh()
 
 
 def setup_curses():
@@ -55,16 +59,56 @@ def check_exit_key(scr):
     return ch == ord('q')
 
 
-def shift_buf(buf):
+def shift_buf(screen_buf):
     pass
 
 
-def draw_pt(buf, x, f=lambda x: 3*math.sin(x*3)):
-    pass
+def draw_amp(screen_buf, x, f=lambda x: 3*math.sin(x*3)):
+    ox = 25
+    last_x = len(screen_buf[0][0]) * 2
+
+    for y in range(int(f(x))):
+        draw_pt(screen_buf, Point(last_x, -y + ox))
 
 
-def show(buf):
-    pass
+def draw_pt(screen_buf, pt):
+    x = int(pt.x / 2)
+    y = curses.LINES - 1 - int(pt.y / 4)
+
+    if pt.y < 0 or y < 0 or pt.x < 0 or x >= curses.COLS - 1:
+        return
+
+    uchar = ord(screen_buf[y][x])
+    screen_buf[y][x] = unicode_char(uchar | relative_uchar(pt))
+
+
+def relative_uchar(pt):
+    bx = int(pt.x) % 2
+    by = int(pt.y) % 4
+
+    if bx == 0:
+        if by == 0:
+            return 0x40
+        else:
+            return 0x4 >> (by - 1)
+    else:
+        if by == 0:
+            return 0x80
+        else:
+            return 0x20 >> (by -1)
+
+
+def unicode_char(param):
+    if sys.version_info[0] == 2:
+        return unichr(param)
+    return chr(param)
+
+
+def show(scr, screen_buf):
+    for num, line in enumerate(screen_buf):
+        scr.addstr(num, 0, u''.join(line).encode('utf-8'))
+
+    scr.refresh()
 
 
 if __name__ == '__main__':
