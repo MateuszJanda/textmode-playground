17096db0d86a8224f6a41b549083c21c3cd9bb0c
Mateusz Janda <mateusz.janda@gmail.com>
2018-12-24 19:31:45 +0100

    Gif converter

--- demos/gif_maker.sh
+++ demos/gif_maker.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+VIDEO_FILE="caputerd_screen.mp4"
+
 if [[ $1 == "-c" || $1 == "--capture" ]]; then
-    ffmpeg -video_size 640x480 -framerate 25 -f x11grab -i :0.0+0,48 output.mp4
+    ffmpeg -video_size 640x480 -framerate 25 -f x11grab -i :0.0+0,48 $VIDEO_FILE
+elif [[ $1 == "-g" || $1 == "--video-to-gif" ]]; then
+    PALETTE_FILE="tmp_pallete.png"
+    OUTPUT_GIF="output.gif"
+    FILTERS="fps=25"
+
+    ffmpeg -v warning -i $VIDEO_FILE -vf "$FILTERS,palettegen" -y $PALETTE_FILE
+    ffmpeg -v warning -i $VIDEO_FILE -i $PALETTE_FILE -lavfi "$FILTERS [x]; [x][1:v] paletteuse" -y $OUTPUT_GIF
+else
+    echo "Warning. No parameters."
+    echo "Usage:"
+    echo "   ./gif_maker.sh -c"
+    echo "   ./gif_maker.sh -g"
 fi
 
-# PALETTE_FILE="tmp_pallete.png"
-# VIDEO_FILE="output_video.mp4"
-# INPUT_FILES="img-%4d.png"
-# OUTPUT_FILE="output.gif"
-# FILTERS="fps=25"
-
-# ffmpeg -r 100 -i $INPUT_FILES -c:v libx264 -crf 0 -preset veryslow $VIDEO_FILE
-# ffmpeg -v warning -i $VIDEO_FILE -vf "$FILTERS,palettegen" -y $PALETTE_FILE
-# ffmpeg -v warning -i $VIDEO_FILE -i $PALETTE_FILE -lavfi "$FILTERS [x]; [x][1:v] paletteuse" -y $OUTPUT_FILE
