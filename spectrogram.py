a38f7a916a9780db57671aa73980d05eec0232bb
Mateusz Janda <mateusz.janda@gmail.com>
2019-04-28 17:21:24 +0200

    Added curses wrapper

--- spectrogram.py
+++ spectrogram.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import locale
+import curses
+import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from scipy import signal
@@ -7,38 +10,82 @@ from scipy.io import wavfile
 import numpy as np
 
 
-def main():
-    # https://pl.wikipedia.org/wiki/Spektrogram
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
+TELEMETRY_MODE = False
+RGB_DIM = 3
+BLACK = (0, 0, 0)
 
+
+def main(scr):
+    # https://pl.wikipedia.org/wiki/Spektrogram
+    setup(scr, enable=True, terminal='/dev/pts/2')
+
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
     # sample_rate - samples per second
     # samples - for 32-bit PCM, values are in range [-2147483648, 2147483647]
     sample_rate, samples = wavfile.read('out.wav')
-    print('Rate:', sample_rate)
-    print('Shape:', samples.shape)
-    print('Time:', samples.shape[0]/sample_rate, '[sec]')
+    log('Rate:', sample_rate)
+    log('Shape:', samples.shape)
+    log('Time:', samples.shape[0]/sample_rate, '[sec]')
 
-    sci_spectrogram(samples, sample_rate)
-    plt_spectogram(samples, sample_rate)
-    plt.show()
+    # sci_spectogram(samples, sample_rate)
+    # plt_spectogram(samples, sample_rate)
+    # plt.show()
 
-    print('Inferno colors data:')
-    print(type(cm.inferno))
-    print(dir(cm.inferno))
-    print(cm.inferno.N)
-    print('Inferno colors:', len(cm.inferno.colors))  # of RGB
+    log('Inferno colors data:')
+    log(type(cm.inferno))
+    log(dir(cm.inferno))
+    log(cm.inferno.N)
+    log('Inferno colors:', len(cm.inferno.colors))  # of RGB
+
+    screen = Screen(scr)
+    log('COLOR_PAIRS', curses.COLOR_PAIRS)
+    # screen.refresh()
 
 
-def sci_spectrogram(samples, sample_rate):
+def setup(scr, enable=False, terminal='/dev/pts/1'):
+    """Main setup function."""
+    setup_curses(scr)
+    setup_telemetry(enable, terminal)
+
+
+def setup_curses(scr):
+    """Setup curses screen."""
+    curses.start_color()
+    curses.use_default_colors()
+    curses.halfdelay(5)
+    curses.noecho()
+    curses.curs_set(False)
+    scr.clear()
+
+
+def setup_telemetry(enable=False, terminal='/dev/pts/1'):
+    """
+    Redirect stderr to other terminal. Run tty command, to get terminal id.
+
+    $ tty
+    /dev/pts/1
+    """
+    global TELEMETRY_MODE
+    TELEMETRY_MODE = enable
+    if TELEMETRY_MODE:
+        sys.stderr = open(terminal, 'w')
+
+
+def log(*args, **kwargs):
+    """log on stderr."""
+    if TELEMETRY_MODE:
+        print(*args, file=sys.stderr)
+
+
+def sci_spectogram(samples, sample_rate):
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.spectrogram.html
     frequencies, times, spectrogram = signal.spectrogram(samples, fs=sample_rate, nfft=1028)
-    print('frequencies.shape', frequencies.shape)
-    print('times.shape', times.shape)
-    print('spectrogram.shape', spectrogram.shape)
-    print('spectrogram value', 10*np.log10(spectrogram[0][1]))
+    log('frequencies.shape', frequencies.shape)
+    log('times.shape', times.shape)
+    log('spectrogram.shape', spectrogram.shape)
+    log('spectrogram value', 10*np.log10(spectrogram[0][1]))
 
     plt.figure(2)
-    # plt.imshow(10*np.log10(spectrogram), aspect='auto', cmap=cm.inferno, origin='lower')
     plt.pcolormesh(times, frequencies, 10*np.log10(spectrogram), cmap=cm.inferno)
     plt.title('signal spectrogram')
     plt.ylabel('Frequency [Hz]')
@@ -53,5 +100,27 @@ def plt_spectogram(samples, sample_rate):
     plt.xlabel('Time [sec]')
 
 
+class Screen:
+    def __init__(self, scr):
+        self._scr = scr
+        buf_shape = (2*curses.LINES, curses.COLS-1, RGB_DIM)
+        self._buf = np.full(shape=buf_shape, fill_value=BLACK)
+
+    def draw_point(self, pos, color):
+        # Don't draw point when they are out of the screen
+        if not (0 <= pos[1] < self._buf[1] and 0 <= pos[0] < self._buf[0]):
+            return
+
+        self._buf[pos[0], pos[1]] = color
+
+    def refresh(self):
+        """Draw buffer content to screen."""
+        dtype = np.dtype('U' + str(self._buf.shape[1]))
+        for num, line in enumerate(self._buf):
+            self._scr.addstr(num, 0, line.view(dtype)[0])
+        self._scr.refresh()
+
+
 if __name__ == '__main__':
-    main()
+    locale.setlocale(locale.LC_ALL, '')
+    curses.wrapper(main)
