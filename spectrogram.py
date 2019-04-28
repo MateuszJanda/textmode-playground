765ea3dcbcfa9ed97b5c693f283ec3570eb60526
Mateusz Janda <mateusz.janda@gmail.com>
2019-04-28 16:38:20 +0200

    Refactor

--- spectrogram.py
+++ spectrogram.py
@@ -1,81 +1,57 @@
+#!/usr/bin/env python3
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from scipy import signal
 from scipy.io import wavfile
 import numpy as np
 
-# https://pl.wikipedia.org/wiki/Spektrogram
 
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
-# sample_rate ilość próbek na sekunde
-# samples - dla 32-bit PCM, będą to liczby w zakresie [-2147483648, 2147483647]
-sample_rate, samples = wavfile.read('out.wav')
-print('Rate:', sample_rate)
-print('Shape:', samples.shape)
-print('Time:', samples.shape[0]/sample_rate, '[sec]') # seconds
+def main():
+    # https://pl.wikipedia.org/wiki/Spektrogram
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
 
-# for s in samples:
-#     print(s)
+    # sample_rate - samples per second
+    # samples - for 32-bit PCM, values are in range [-2147483648, 2147483647]
+    sample_rate, samples = wavfile.read('out.wav')
+    print('Rate:', sample_rate)
+    print('Shape:', samples.shape)
+    print('Time:', samples.shape[0]/sample_rate, '[sec]')
 
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.spectrogram.html
-frequencies, times, spectrogram = signal.spectrogram(samples, fs=sample_rate, nfft=1028)
-print('frequencies.shape', frequencies.shape)
-print('times.shape', times.shape)
-print('spectrogram.shape', spectrogram.shape)
+    sci_spectrogram(samples, sample_rate)
+    plt_spectogram(samples, sample_rate)
+    plt.show()
 
-print('spectrogram value', 10*np.log10(spectrogram[0][1]))
-# for x in frequencies:
-#     print(x)
-
-# plt.plasma()
-
-plt.figure(figsize=(5, 4))
-# plt.imshow(spectrogram, aspect='auto', cmap=cm.inferno, origin='lower')
-plt.imshow(10*np.log10(spectrogram), aspect='auto', cmap=cm.inferno, origin='lower')
-plt.title('Spectrogram 1')
-plt.ylabel('Frequency band')
-plt.xlabel('Time window')
-plt.tight_layout()
-plt.show()
+    print('Inferno colors data:')
+    print(type(cm.inferno))
+    print(dir(cm.inferno))
+    print(cm.inferno.N)
+    print('Inferno colors:', len(cm.inferno.colors))  # of RGB
 
 
-print('Inferno colors data:')
-print(type(cm.inferno))
-print(dir(cm.inferno))
-print(cm.inferno.N)
+def sci_spectrogram(samples, sample_rate):
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.spectrogram.html
+    frequencies, times, spectrogram = signal.spectrogram(samples, fs=sample_rate, nfft=1028)
+    print('frequencies.shape', frequencies.shape)
+    print('times.shape', times.shape)
+    print('spectrogram.shape', spectrogram.shape)
+    print('spectrogram value', 10*np.log10(spectrogram[0][1]))
+
+    plt.figure(2)
+    # plt.imshow(10*np.log10(spectrogram), aspect='auto', cmap=cm.inferno, origin='lower')
+    plt.pcolormesh(times, frequencies, 10*np.log10(spectrogram), cmap=cm.inferno)
+    plt.title('signal spectrogram')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
 
 
-# for k, v in cm.inferno.item():
-    # print(k, v)
-print('Inferno colors:', len(cm.inferno.colors)) # of RGB
+def plt_spectogram(samples, sample_rate):
+    plt.figure(3)
+    plt.specgram(samples, Fs=sample_rate, cmap=cm.inferno, NFFT=1028)
+    plt.title('matplotlib sectrogram')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
 
 
-# plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram), animated=True)
-# plt.pcolormesh(times, frequencies, spectrogram, animated=True)
-
-
-
-
-# plt.imshow(spectrogram, cmap=cm.plasma)
-plt.specgram(samples, Fs=sample_rate, cmap=cm.inferno, NFFT=1028)
-plt.title('Spectrogram 2')
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.show()
-
-
-# x = np.cumsum(np.random.random(1000) - 0.5)
-
-# print(samples.shape)
-# print(type(samples))
-# print((spectrogram.shape))
-
-# fig, (ax1, ax2) = plt.subplots(nrows=2)
-# data, freqs, bins, im = ax1.specgram(x)
-# ax1.axis('tight')
-
-# # "specgram" actually plots 10 * log10(data)...
-# ax2.pcolormesh(bins, freqs, 10 * np.log10(data))
-# ax2.axis('tight')
-
-# plt.show()
+if __name__ == '__main__':
+    main()
