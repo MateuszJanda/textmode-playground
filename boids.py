f3418b94f6e02145fdc7327afcdb2f97aa1532e6
Mateusz Janda <mateusz.janda@gmail.com>
2019-06-04 12:33:03 +0200

    Update

--- boids.py
+++ boids.py
@@ -9,6 +9,7 @@ Ad maiorem Dei gloriam
 import curses
 import locale
 import time
+import math
 import numpy as np
 
 
@@ -25,8 +26,9 @@ MAX_VEL = 4
 
 class Body:
     def __init__(self):
-        self.pos = np.random.randint(0, 400, [2, 1])
-        self.vel = np.random.randint(0, 2, [2, 1])
+        self.pos = np.random.uniform(0, 400, [2, 1])
+        self.vel = np.random.uniform(0, 2, [2, 1])
+        self.l = 0
 
 
 def main():
@@ -47,11 +49,62 @@ def main():
                 if b1 is b2:
                     continue
 
-                dist = math.sqrt((b1.pos[0] - b2.pos[0])**2 + (b1.pos[1] - b2.pos[1])**2)
-                k = b1.vel[0] / math.sqrt(b1.vel[0]*b1.vel[0] + b1.vel[1]*b1.vel[1]) *
-                    ((b2.x - b1.x) / dist) +
-                    b1.vel[1] / math.sqrt(b1.vel[0]*b1.vel[0] + b1.vel[1]*b1.vel[1]) *
-                    ((b2.y - b1.y) / dist)
+                dist = math.sqrt((b1.pos[1] - b2.pos[1])**2 + (b1.pos[0] - b2.pos[0])**2)
+                k = b1.vel[1] / math.sqrt(b1.vel[1]**2 + b1.pos[0]**2) *
+                    ((b2.pos[1] - b1.pos[1]) / dist) +
+                    b1.pos[0] / math.sqrt(b1.vel[1]**2 + b1.pos[0]**2) *
+                    ((b2.pos[0] - b1.pos[0]) / dist)
+                if k < -1:
+                    k = -1
+                elif k > 0:
+                    k = 1
+                k = math.fabs(180*math.acos(k)) / maht.pi
+                if dist < NEIGHBORHOOD_RADIUS and k > VIEWING_ANGLE:
+                    b1.l += 1
+                    b1.avg_vel += b2.vel
+                    b1.avg_dist += dist
+
+        for b1 in bodies:
+            b1.vel += NEIGHBORHOOD_VEL_WEIGHT * ((b1.avg_vel / b1.l) - b1.vel)
+            b1.vel += NOISE_WEIGHT * (np.random.uniform(0, 0.5, [2, 1]) * MAX_VEL)
+            if b1.l > 1:
+                b1.avg_dist /= b1.l - 1
+
+            for b2 in bodies:
+                if b1 is b2:
+                    continue
+
+                dist = math.sqrt((b1.pos[1] - b2.pos[1])**2 + (b1.pos[0] - b2.pos[0])**2)
+                k = b1.vel[1] / math.sqrt(b1.vel[1]**2 + b1.vel[0]**2) *
+                    ((b2.pos[1] -b1.pos[1]) / dist) +
+                    b1.vel[0] / math.sqrt(b1.vel[1]**2 + b1.vel[0]**2) *
+                    ((b2.pos[0] - b1.pos[0]) / dist)
+
+                if k < -1:
+                    k = -1
+                elif k > 1:
+                    k = 1
+                k = math.fabs(180*math.acos(k)) / maht.pi
+                if dist < NEIGHBORHOOD_RADIUS and k > VIEWING_ANGLE:
+                    if math.fabs(b2.pos[1] - b1.pos[1]) > MIN_DISTANCE:
+                        b1.vel += (NEIGHBORHOOD_DIST_WEIGHT / b1.l) * (((b2.pos - b1.pos) * (dist - b1.avg_dist)) / dist)
+
+                    else:
+                        b1.vel += (MIN_DIST_WEIGHT / b1.l) * (((b2.pos - b1.pos) * MIN_DISTANCE) / dist) - (b2.pos - b1.pos)
+
+
+            if math.sqrt(b1.vel[1]**2 + b1.vel[0]**2) > MAX_VEL:
+                b1.vel = 0.75 * b1.vel
+
+        for b in bodies:
+            if b1.vel[1] < 0:
+                b1.vel[1] += 450
+            if b1.vel[0] < 0:
+                b1.vel[0] += 400
+            if b1.vel[1] > 450:
+                b1.vel[1] -= 450
+            if v1.vel[0] > 400:
+                b1.vel[0] -= 400
 
 
         time.sleep(0.2)
