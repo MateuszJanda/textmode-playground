d409014c7acf885bdb314989390fee78ede22f4a
Mateusz Janda <mateusz.janda@gmail.com>
2019-06-28 23:26:23 +0200

    Simple wave

--- wave_signal.py
+++ wave_signal.py
@@ -8,104 +8,66 @@ Ad maiorem Dei gloriam
 
 import sys
 import curses
-import math
-import random
-from time import sleep
-import collections
+import time
 import locale
 
 
 BLANK_BRAILLE = u'\u2800'
-
-Point = collections.namedtuple('Point', ['x', 'y'])
+WAVE = '''`-._,-'"'''
 
 
 def main(scr):
-    setup_curses()
-    scr.clear()
+    setup_curses(scr)
 
-    screen_buf = clean_buf()
-    x = 0
+    step = 0
+    while True:
+        simple_wave(scr, step, length=10)
 
-    while not check_exit_key(scr):
-        shift_buf(screen_buf)
-        draw_amp(screen_buf, x)
-        show(scr, screen_buf)
-
-        x += 1
-        sleep(0.1)
+        step += 1
+        scr.refresh()
+        time.sleep(0.1)
 
 
-def setup_curses():
+def setup_curses(scr):
     curses.use_default_colors()
     curses.halfdelay(1)
     curses.curs_set(False)
+    scr.clear()
 
 
-def clean_buf():
-    return [list(BLANK_BRAILLE * (curses.COLS - 1)) for _ in range(curses.LINES)]
+def setup_stderr(terminal='/dev/pts/1'):
+    """
+    Redirect stderr to other terminal. Run tty command, to get terminal id.
+
+    $ tty
+    /dev/pts/1
+    """
+    sys.stderr = open(terminal, 'w')
 
 
-def check_exit_key(scr):
-    """ Wait for key (defined by halfdelay), and check if q """
+def log(*args, **kwargs):
+    """log on stderr."""
+    print(*args, file=sys.stderr)
+
+
+def is_exit_key(scr):
+    """Wait for key (defined by halfdelay), and check if q."""
     ch = scr.getch()
     return ch == ord('q')
 
 
-def shift_buf(screen_buf):
-    pass
+def simple_wave(scr, step, length=len(WAVE)):
+    # Length which fully contains user wave
+    basic_length = length // len(WAVE) + 1
+    # One extra wave for shifting
+    wave = WAVE * (basic_length + 1)
 
+    shift = step % len(WAVE)
+    wave = '[' + wave[shift:shift+length] + ']'
 
-def draw_amp(screen_buf, x, f=lambda x: 3*math.sin(x*3)):
-    ox = 25
-    last_x = len(screen_buf[0][0]) * 2
-
-    for y in range(int(f(x))):
-        draw_pt(screen_buf, Point(last_x, -y + ox))
-
-
-def draw_pt(screen_buf, pt):
-    x = int(pt.x / 2)
-    y = curses.LINES - 1 - int(pt.y / 4)
-
-    if pt.y < 0 or y < 0 or pt.x < 0 or x >= curses.COLS - 1:
-        return
-
-    uchar = ord(screen_buf[y][x])
-    screen_buf[y][x] = unicode_char(uchar | relative_uchar(pt))
-
-
-def relative_uchar(pt):
-    bx = int(pt.x) % 2
-    by = int(pt.y) % 4
-
-    if bx == 0:
-        if by == 0:
-            return 0x40
-        else:
-            return 0x4 >> (by - 1)
-    else:
-        if by == 0:
-            return 0x80
-        else:
-            return 0x20 >> (by -1)
-
-
-def unicode_char(param):
-    if sys.version_info[0] == 2:
-        return unichr(param)
-    return chr(param)
-
-
-def show(scr, screen_buf):
-    for num, line in enumerate(screen_buf):
-        scr.addstr(num, 0, u''.join(line).encode('utf-8'))
-
-    scr.refresh()
+    scr.addstr(0, 0, wave)
 
 
 if __name__ == '__main__':
     locale.setlocale(locale.LC_ALL, '')
-    # locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     curses.wrapper(main)
-
