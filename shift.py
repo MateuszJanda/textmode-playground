f0027678be5d4a8b1f54f8ff466fdb7e4fcec988
Mateusz Janda <mateusz.janda@gmail.com>
2019-07-01 19:37:44 +0200

    Fixed create_shift_arr

--- shift.py
+++ shift.py
@@ -53,12 +53,12 @@ def main(scr):
     assert orig_arr.shape[0] % HEIGHT == 0
     assert orig_arr.shape[1] % WIDTH == 0
 
-    arr1 = create_shift_arr(orig_arr, shift_x=2, shift_y=2)
-    arr2 = create_shift_arr(orig_arr, shift_x=-2, shift_y=-2)
+    arr2 = create_shift_arr(orig_arr, shift_x=2, shift_y=2)
+    arr3 = create_shift_arr(orig_arr, shift_x=0, shift_y=-4)
 
     dots_arr1 = create_dots_arr(orig_arr)
-    dots_arr2 = create_dots_arr(arr1)
-    dots_arr3 = create_dots_arr(arr2)
+    dots_arr2 = create_dots_arr(arr2)
+    dots_arr3 = create_dots_arr(arr3)
 
     draw(scr, dots_arr1, curses.color_pair(WHITE_ID))
     draw(scr, dots_arr2, curses.color_pair(RED_ID))
@@ -104,20 +104,24 @@ def log(*args, **kwargs):
 
 def create_shift_arr(orig_arr, shift_x, shift_y):
     new_arr = np.copy(orig_arr)
-    wall_x = np.full(shape=(orig_arr.shape[0], abs(shift_x)), fill_value=0)
 
-    if shift_x < 0:
-        new_arr = np.concatenate((wall_x, orig_arr), axis=1)
-    else:
-        new_arr = np.concatenate((orig_arr, wall_x), axis=1)
-    new_arr = np.delete(orig_arr, shift_x, axis=1)
+    if shift_x:
+        wall_x = np.full(shape=(new_arr.shape[0], abs(shift_x)), fill_value=0)
+        if shift_x > 0:
+            new_arr = np.concatenate((wall_x, new_arr), axis=1)
+            new_arr = np.delete(new_arr, np.s_[new_arr.shape[1]-shift_x:], axis=1)
+        else:
+            new_arr = np.concatenate((new_arr, wall_x), axis=1)
+            new_arr = np.delete(new_arr, np.s_[:-shift_x], axis=1)
 
-    wall_y = np.full(shape=(abs(shift_y), new_arr.shape[1]), fill_value=0)
-    if shift_y < 0:
-        new_arr = np.concatenate((wall_y, new_arr), axis=0)
-    else:
-        new_arr = np.concatenate((new_arr, wall_y), axis=0)
-    new_arr = np.delete(new_arr, shift_y, axis=1)
+    if shift_y:
+        wall_y = np.full(shape=(abs(shift_y), new_arr.shape[1]), fill_value=0)
+        if shift_y > 0:
+            new_arr = np.concatenate((wall_y, new_arr), axis=0)
+            new_arr = np.delete(new_arr, np.s_[new_arr.shape[0]-shift_y:], axis=0)
+        else:
+            new_arr = np.concatenate((new_arr, wall_y), axis=0)
+            new_arr = np.delete(new_arr, np.s_[:-shift_y], axis=0)
 
     return new_arr
 
