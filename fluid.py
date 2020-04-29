81d3708a580e60a157e978d98ae1b337b131f993
Mateusz Janda <mateusz.janda@gmail.com>
2020-04-29 12:25:48 +0200

    Added fluid.py

---
+++ fluid.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Author: Mateusz Janda <mateusz janda at gmail com>
+Site: github.com/MateuszJanda
+Ad maiorem Dei gloriam
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
