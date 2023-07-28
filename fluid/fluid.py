89659add4bdff47700381e505bac8933f4d97bf3
Mateusz Janda <mateusz.janda@gmail.com>
2023-07-28 15:53:22 +0200

    Update credits

--- fluid/fluid.py
+++ fluid/fluid.py
@@ -10,9 +10,10 @@ Credits:
   https://pdfs.semanticscholar.org/847f/819a4ea14bd789aca8bc88e85e906cfc657c.pdf
 - "Fluid Simulation for Dummies" by Mike Ash
   https://mikeash.com/pyblog/fluid-simulation-for-dummies.html
-- "Coding Challenge #132: Fluid Simulation"
+- "But How DO Fluid Simulations Work?" by Gonkee
+  https://www.youtube.com/watch?v=qsYE1wMEMPA
+- "Coding Challenge #132: Fluid Simulation" by Daniel Shiffman
   https://www.youtube.com/watch?v=alhpH6ECFvQ
-- "Coding Challenge #132: Fluid Simulation"
   https://github.com/CodingTrain/website/tree/master/CodingChallenges/CC_132_FluidSimulation/Processing/CC_132_FluidSimulation
 """
 
