99142a32cf4d9a4cddf28bba9bb3ac6d2b83d4b5
Mateusz Janda <mateusz.janda@gmail.com>
2023-07-30 17:46:07 +0200

    Fixed fading

--- digital_rain/src/main.rs
+++ digital_rain/src/main.rs
@@ -11,14 +11,10 @@ use std::{thread, time};
 
 use rand::distributions::Alphanumeric;
 use rand::Rng;
-
-// macro_rules! csi {
-//     ($( $l:expr ),*) => { concat!("\x1B[", $( $l ),*) };
-
-// }
+use  std::collections::VecDeque;
 
 #[derive(Copy, Clone, Debug)]
-pub struct RichColor;
+pub struct RichColor(pub usize);
 
 impl Color for RichColor {
     /// Example:
@@ -39,7 +35,10 @@ impl RichColor {
     /// Returns the ANSI escape sequence as a string.
     pub fn fg_str(&self) -> &'static str {
         // csi!("38;2;", $value, "m")
-        "\x1B[38;2;255;255;0m"
+        match self.0 {
+            0 => "\x1B[38;2;255;255;0m",
+            _ => "\x1B[38;2;255;0;0m",
+        }
     }
 
     #[inline]
@@ -48,12 +47,6 @@ impl RichColor {
         // csi!("48;2;", $value, "m")
         "\x1B[48;2;0;255;255m"
     }
-}
-
-#[derive(Clone, Debug)]
-struct Cell {
-    ch: char,
-    dt: u32,
 }
 
 #[derive(Clone, Debug)]
@@ -75,40 +68,46 @@ impl DigitDrop {
         }
     }
 
-    // fn print(&self) {
-    //     print!("{}{}", termion::cursor::Goto(self.x, self.y), self.ch);
-    // }
-
-    // fn text(&self) -> String {
-    //     format!(
-    //         "{}{}",
-    //         termion::cursor::Goto(self.x as u16 + 1, self.y as u16 + 1),
-    //         self.ch
-    //     )
-    // }
-
-    fn print(&self, screen: &mut AlternateScreen<Stdout>) {
+    fn print_drop(&self, screen: &mut AlternateScreen<Stdout>) {
         write!(
             screen,
             "{}{}{}",
             termion::cursor::Goto(self.x as u16 + 1, self.y as u16 + 1),
-            termion::color::Fg(RichColor),
+            termion::color::Fg(RichColor(0)),
             self.ch
         )
         .unwrap();
     }
 
-    fn fall_down(&mut self, buffer: &mut Vec<Vec<char>>) {
-        self.y += 1;
-        if self.y < buffer.len() {
-            buffer[self.y ][self.x] = self.ch;
+    fn print_fade(&self, screen: &mut AlternateScreen<Stdout>, fade: usize) {
+        if fade == FADE_LEN {
+            write!(
+                screen,
+                "{} ",
+                termion::cursor::Goto(self.x as u16 + 1, self.y as u16 + 1),
+            )
+            .unwrap();
+        } else {
+            write!(
+                screen,
+                "{}{}{}",
+                termion::cursor::Goto(self.x as u16 + 1, self.y as u16 + 1),
+                termion::color::Fg(RichColor(fade)),
+                self.ch
+            )
+            .unwrap();
         }
     }
 
-    // fn go_down(&mut self) {
-    //     self.old_y = self.y;
-    //     self.y += 1;
-    // }
+    fn fall_down(&mut self, buffer: &mut Vec<Vec<char>>) -> bool {
+        self.y += 1;
+        if self.y < buffer.len() {
+            buffer[self.y][self.x] = self.ch;
+            return true;
+        }
+
+        false
+    }
 
     fn is_in_buffer(&self, buffer: &Vec<Vec<char>>) -> bool {
         buffer[self.y][self.x] == self.ch
@@ -151,63 +150,63 @@ fn main() {
 
     // Get terminal size
     let (num_cols, num_rows) = termion::terminal_size().unwrap();
-    // let mut buffer = vec![vec![Cell { ch: ' ', dt: 0 }; num_cols as usize]; num_cols as usize];
+
     let mut buffer: Vec<Vec<char>> = vec![vec![' '; num_cols as usize]; num_cols as usize];
-    let mut fades: Vec<Vec<DigitDrop>> = vec![];
-
-    // println!("Hello, world!");
-    // println!("Size {:?}", termion::terminal_size());
-    // // println!("Size {:?}", termion::terminal_size_pixels());
-    // println!("{}Blue", color::Fg(color::Blue));
-    // println!("{}{}RichColor", termion::cursor::Goto(5, 3), color::Fg(RichColor));
-
+    let mut fades: VecDeque<Vec<DigitDrop>> = VecDeque::new();
     // Init digital drops
     let mut digit_drops = vec![];
+
     for x in 0..num_cols as usize {
-        if rand::thread_rng().gen_range(0..10) < 5 {
+        if rand::thread_rng().gen_range(0..10) < 7 {
             digit_drops.push(DigitDrop::new(x, 0, num_rows as usize));
         }
     }
 
+    // digit_drops.push(DigitDrop::new(1, 0, num_rows as usize));
+    // digit_drops.push(DigitDrop::new(3, 0, num_rows as usize));
+
     loop {
         // Fall drops
+        let mut v: Vec<DigitDrop> = vec![];
         for digit_drop in digit_drops.iter_mut() {
-            if rand::thread_rng().gen_range(0..10) < 5 {
-                digit_drop.fall_down(&mut buffer);
+            // if rand::thread_rng().gen_range(0..10) < 5 {
+            if true {
+                let is_fall = digit_drop.fall_down(&mut buffer);
+
+                if is_fall {
+                    v.push(digit_drop.clone());
+                }
             }
-        }
-
-        // Draw digital drops
-        for drop in digit_drops.iter() {
-            drop.print(&mut screen);
-        }
-
-        // Removed overalping fading
-        for f in fades.iter_mut() {
-            f.retain(|d| d.is_in_buffer(&buffer) );
-        }
-
-        // Draw fading
-        for drops in fades.iter() {
-            for drop in drops.iter() {
-                drop.print(&mut screen);
-            }
-        }
-        // Remove last fading
-        if fades.len() == FADE_LEN {
-            fades.pop();
         }
 
         // Remove all drops that are out of the screen
         digit_drops.retain(|digit_drop| !digit_drop.is_out_of_screen());
 
+        // Draw digital drops
+        for drop in digit_drops.iter() {
+            drop.print_drop(&mut screen);
+        }
+
+        // Removed overalping fading
+        for f in fades.iter_mut() {
+            f.retain(|d| d.is_in_buffer(&buffer));
+        }
+
+        // Draw fading
+        for (f, drops) in fades.iter().enumerate() {
+            for drop in drops.iter() {
+                drop.print_fade(&mut screen, f + 1);
+            }
+        }
+
+        // Remove last fading
+        if fades.len() == FADE_LEN {
+            fades.pop_back();
+        }
+
 
         // Copy digital drops to fades
-        let mut v: Vec<DigitDrop> = vec![];
-        for d in digit_drops.iter() {
-            v.push(d.clone());
-        }
-        fades.push(v);
+        fades.push_front(v);
 
         // Add new drops
         if digit_drops.len() < 50 {
@@ -219,6 +218,6 @@ fn main() {
         }
 
         screen.flush().unwrap();
-        thread::sleep(time::Duration::from_millis(5));
+        thread::sleep(time::Duration::from_millis(100));
     }
 }
