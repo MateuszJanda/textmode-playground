c3b3d97e1cfdbe213cf35bd542c130e9a1df59d7
Mateusz Janda <mateusz.janda@gmail.com>
2023-08-04 13:57:19 +0200

    Update unicode_support.py

--- tools/unicode_support.py
+++ tools/unicode_support.py
@@ -4,42 +4,31 @@
 # Site: github.com/MateuszJanda/textmode-playground
 # Ad maiorem Dei gloriam
 
-import curses
-import locale
-import fontconfig
-
-"""
-Useful links:
-http://stackoverflow.com/questions/1279341/how-do-i-use-extended-characters-in-pythons-curses-library
-https://en.wikipedia.org/wiki/Braille_Patterns
-"""
-
-def main(scr):
-    scr.clear()
-    scr.addstr(0, 0, u'\u3042'.encode('utf-8'))
-    scr.addstr(1, 0, u'\u2875'.encode('utf-8'))
-    scr.addstr(2, 0, 'asdf')
-
-    char_support(scr)
-
-    scr.refresh()
-    scr.getch()
+import subprocess
 
 
-def char_support(scr, character=chr(0x28a7)):
-    font_names = fontconfig.query()
-    row = 4
-
-    for name in font_names:
-        font = fontconfig.FcFont(name)
-        if font.has_char(character) and font.fullname:
-            # info = font.file
-            # info = font.fullname[0][1] + ' [' + str(font.spacing) + ']'
-            info = str(font.fullname)
-            scr.addstr(row, 0, info)
-            row += 1
+def main():
+    # https://en.wikipedia.org/wiki/Braille_Patterns
+    check_unicode_support(0x28A7)
+    check_unicode_support(ord("ぃ"))
 
 
-if __name__ == '__main__':
-    locale.setlocale(locale.LC_ALL, '')
-    curses.wrapper(main)
+def check_unicode_support(ch: int) -> None:
+    """
+    This function use fontconfig command line tools. Check if unicode character is support by
+    available fonts.
+    """
+
+    charset = f"{ch:x}"
+    # result = subprocess.run(["fc-list", f":charset={charset}:scalable=true:spacing=mono:", "file",
+    result = subprocess.run(
+        ["fc-list", f":charset={charset}", "file", "family", "fontformat"],
+        stdout=subprocess.PIPE,
+    )
+    print(f"Character '{chr(ch)}' [0x{ch:x}] supported by:")
+    for line in result.stdout.decode("utf-8").strip().split("\n"):
+        print(line)
+
+
+if __name__ == "__main__":
+    main()
