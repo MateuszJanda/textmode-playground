9204c877450640efecf8a1b17deb59dc7e933d54
Mateusz Janda <mateusz.janda@gmail.com>
2023-08-07 22:31:40 +0200

    Trimming image

--- tools/img_to_ansi.py
+++ tools/img_to_ansi.py
@@ -8,15 +8,32 @@ import cv2
 import numpy as np
 
 
-def main():
-    input_arr = get_input_array(args.file)
+BLOCK_WIDTH = 2
+BLOCK_HEIGHT = 2
 
-def get_input_array(path):
-    """
-    Get input image (gray scale) as numpy array.
-    """
-    gray_img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    return gray_img
+def main():
+    file_name = ""
+    input_arr = read_img_in_grayscale(file_name)
+
+    height = (input_arr.shape[0] // BLOCK_HEIGHT) * BLOCK_HEIGHT
+    width = (input_arr.shape[1] // BLOCK_WIDTH) * BLOCK_WIDTH
+
+    # Truncate rows and columns
+    input_arr = input_arr[:height, :width]
+
+    small_arr = input_arr.reshape([BLOCK_HEIGHT, input_arr.shape[0]//BLOCK_HEIGHT, BLOCK_WIDTH, input_arr.shape[1]//BLOCK_WIDTH]).mean(3).mean(1)
+
+    for row in small_arr:
+        for pixel in row:
+            u = 50
+            d = 150
+            f"\033[38;2;{u};{u};{u}m\033[48;2;{d};{d};{d}m▄"
+
+
+def read_img_in_grayscale(path):
+    """Get input image (gray scale) as numpy array."""
+    return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
 
 if __name__ == "__main__":
     main()
