80bbbc1a4cee6675b6b05d63f35fd8b02fda3374
Mateusz Janda <mateusz.janda@gmail.com>
2023-08-10 20:32:23 +0200

    Added CharComparator class

--- tools/char_cmp.py
+++ tools/char_cmp.py
@@ -9,32 +9,39 @@ from PIL import Image, ImageDraw, ImageFont
 import cv2
 import numpy as np
 
+IMG_HEIGHT = 17
+IMG_WIDTH = 14
+
 
 def main() -> None:
-    dist("x", "X")  # Ok
-    dist("a", "X")  # Error
+    cmp = CharComparator()
+    cmp.dist("x", "X")  # Ok
+    cmp.dist("a", "X")  # Error
 
 
-def create_img(ch: str, pos_x: int = 3, pos_y: int = 2) -> np.ndarray:
-    IMG_SHAPE = (17, 14)
-    img = Image.new("L", color=0, size=(IMG_SHAPE[1], IMG_SHAPE[0]))
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("DejaVuSansMono", size=12)
-    draw.text(xy=(pos_x, pos_y), text=ch, fill=255, font=font, spacing=0)
+class CharComparator:
+    def __init__(self) -> None:
+        self.font = ImageFont.truetype("DejaVuSansMono", size=12)
 
-    img.save(f"{ch}.png")
-    return np.array(img)
+    def _create_img(self, ch: str, start_x: int = 3, start_y: int = 3) -> np.ndarray:
+        """Draw character glyph image."""
+        img = Image.new("L", color=0, size=(IMG_WIDTH, IMG_HEIGHT))
+        draw = ImageDraw.Draw(img)
+        draw.text(xy=(start_x, start_y), text=ch, fill=255, font=self.font, spacing=0)
+        # img.save(f"{ch}.png")
+        # return np.array(img)
+        return img
 
-
-def dist(ch1: str, ch2: str) -> None:
-    img1 = create_img(ch1)
-    img2 = create_img(ch2)
-    con1, _ = cv2.findContours(img1, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
-    con2, _ = cv2.findContours(img2, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
-    hd = cv2.createHausdorffDistanceExtractor()
-    sd = cv2.createShapeContextDistanceExtractor()
-    print(f"{ch1} <-> {ch2}: hd {hd.computeDistance(con1[0], con2[0])}")
-    print(f"{ch1} <-> {ch2}: sd {sd.computeDistance(con1[0], con2[0])}")
+    def dist(self, ch1: str, ch2: str) -> None:
+        """Calculate distance between two characters glyph."""
+        img1 = self._create_img(ch1)
+        img2 = self._create_img(ch2)
+        con1, _ = cv2.findContours(img1, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
+        con2, _ = cv2.findContours(img2, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
+        hd = cv2.createHausdorffDistanceExtractor()
+        sd = cv2.createShapeContextDistanceExtractor()
+        print(f"HD: {ch1} <-> {ch2} == {hd.computeDistance(con1[0], con2[0])}")
+        print(f"SD: {ch1} <-> {ch2} == {sd.computeDistance(con1[0], con2[0])}")
 
 
 if __name__ == "__main__":
